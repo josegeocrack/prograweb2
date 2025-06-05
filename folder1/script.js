533e0519446ff55649f854a1d6fd3226f15cde80
Josefina <jgeoghegan@itba.edu.ar>
2025-06-05 19:31:28 -0300

    borro agropecuaria - me quedo con libros

--- folder1/script.js
+++
@@ -1,8 +0,0 @@
-function mostrarMensaje() {
-  alert("¡Gracias por visitarnos! Te invitamos a recorrer nuestra historia.");
-}
-
-document.getElementById('formContacto').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert("Mensaje enviado correctamente. ¡Gracias por contactarnos!");
-});
